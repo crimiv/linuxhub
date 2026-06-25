9ff208cd5db4ebdf90dfbb5d82eb0a1e83485a86
coolio <bj6503735@gmail.com>
2026-06-25 05:13:26 +0000

    Update init.lua

--- games/mm2/init.lua
+++ games/mm2/init.lua
@@ -34,6 +34,7 @@ AppleHub = AppleHub or {}
 AppleHub.WindUI = WindUI
 AppleHub.Utils = utils
 AppleHub.Config = config
+AppleHub.Toggles = AppleHub.Toggles or {}
 
 local version = APPLE_HUB_VERSION or "1.0.0"
 
@@ -69,9 +70,11 @@ task.spawn(function()
         if remoteVersion and remoteVersion ~= version then
             WindUI:Notify({
                 Title = "Update Available",
-                Content = "New version " .. remoteVersion .. " is available. Please reload the hub.",
-                Duration = 5,
+                Content = "New version " .. remoteVersion .. " is available. Reloading...",
+                Duration = 3,
             })
+            task.wait(1)
+            loadstring(game:HttpGet(BASE_URL .. "main.lua"))()
         end
     end
 end)
@@ -81,3 +84,10 @@ LoadScript("games/mm2/combat.lua")
 LoadScript("games/mm2/troll.lua")
 LoadScript("games/mm2/misc.lua")
 LoadScript("games/mm2/settings.lua")
+
+if _G.APPLE_HUB_STATES then
+    for key, value in pairs(_G.APPLE_HUB_STATES) do
+        AppleHub.Toggles[key] = value
+    end
+    _G.APPLE_HUB_STATES = nil
+end
