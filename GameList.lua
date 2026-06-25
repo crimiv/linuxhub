aac261464101c213e53431bbf210158aa4391ca6
coolio <bj6503735@gmail.com>
2026-06-24 21:25:32 -0500

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [142823291] = "https://raw.githubusercontent.com/crimiv/silverhub/refs/heads/main/Games/Murder%20Mystery%202.lua", -- Murder Mystery 2
+  [142823291] = "https://raw.githubusercontent.com/crimiv/silverhub/refs/heads/main/Games/Murder%20Mystery%202.lua" -- Murder Mystery 2
 }
 
 return Games
