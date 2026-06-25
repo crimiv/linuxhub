00f6da5060f10ea3d76e22b3dd936b27597e126c
coolio <bj6503735@gmail.com>
2026-06-24 21:45:09 -0500

    Update utils.lua

--- shared/utils.lua
+++ shared/utils.lua
@@ -1,1 +1,33 @@
+local SilverHub = {}
 
+function SilverHub.GetPlayerFromArg(arg)
+    if typeof(arg) == "Instance" and arg:IsA("Player") then
+        return arg
+    elseif type(arg) == "string" then
+        return game.Players:FindFirstChild(arg)
+    end
+    return nil
+end
+
+function SilverHub.PlayerHasTool(player, toolName)
+    if not player then return false end
+    local backpack = player:FindFirstChild("Backpack")
+    if backpack then
+        for _, tool in ipairs(backpack:GetChildren()) do
+            if tool:IsA("Tool") and tool.Name == toolName then
+                return true
+            end
+        end
+    end
+    local character = player.Character
+    if character then
+        for _, tool in ipairs(character:GetChildren()) do
+            if tool:IsA("Tool") and tool.Name == toolName then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+return SilverHub
