3c0f7bb590cbb6616f96e8ce9adda46ea762a5a0
coolio <bj6503735@gmail.com>
2026-07-01 16:33:02 +0000

    Update init.lua

--- games/gs/init.lua
+++ games/gs/init.lua
@@ -98,6 +98,8 @@ end
 
 LoadScript("games/gs/combat.lua")
 LoadScript("games/gs/autofarm.lua")
+LoadScript("games/universal/admin.lua")
+LoadScript("games/universal/misc.lua")
 
 if _G.LINUXHUB_STATES then
     for key, value in pairs(_G.LINUXHUB_STATES) do
